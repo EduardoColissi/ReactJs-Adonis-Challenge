08772f60ac1f0b0b4046609bc6bd3ee1894085fb
EduardoColissi <educolissi@hotmail.com>
2022-01-31 14:42:55 -0300

    feat/style: created basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
+import "antd/dist/antd.css";
+import { Layout, Menu } from "antd";
+import { Input } from "antd";
+
+const { Search } = Input;
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Layout className="layout">
+      <Header style={{ backgroundColor: "white" }}>
+        <Menu mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu.Item>Todos os Livros</Menu.Item>
+          <Menu.Item>Livros Favoritos</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: "0 50px", marginTop: "50px" }}>
+        <div className="site-layout-content">
+          <Search
+            placeholder="Pesquise livros..."
+            style={{ width: "300px" }}
+            enterButton
+          />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Created by Eduardo Colissi Wiceskoski ©2022
+      </Footer>
+    </Layout>
   );
 }
 
